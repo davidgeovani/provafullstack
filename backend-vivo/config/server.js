160f384761d49686d1e5e66c83b7820fe501a285
davidgeovani <davidgeovani1@gmail.com>
2021-04-27 06:50:27 -0300

    v0.1

---
+++ backend-vivo/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5829a4832c07aeda9d5eea796bb05b00'),
+    },
+  },
+});
---
+++ backend-vivo/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bee23105-c21f-4029-a510-d0beee29ded1'
+};
